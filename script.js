9113d438e553adceb745031d2fcba400365502c7
KashafDawood <kashaf.dawood@gmail.com>
2022-10-22 20:25:01 +0500

    mobile Menu complete

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const burger = document.getElementById("burger");
 const mobileMenu = document.getElementById("MnavBar");
 
+
+const navLink = Array.from(document.getElementsByClassName("navLink"));
+
+for(let i = 0; i < navLink.length; i++){
+    navLink[i].addEventListener("click" , function(){
+        mobileMenu.style.right = "-750px";
+    });
+}
+
     mobileMenu.style.right = "-750px";
 
     burger.onclick = function(){
